1539036c0da4d6e59a19ff4cbe562d35c2ad6982
brennovianacoelho <brenno.viana@smartnx.com>
2024-11-14 12:10:17 -0300

    Adicionando logica para salvar imagem

--- backend/services/auth/src/app/app.js
+++ backend/services/auth/src/app/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { userRoutes } from "../users/v1/routes/userRoutes.js";
 import cors from "cors";
+import path from "path";
 // import { authenticateJWT } from "../generic-middlewares/authenticateJWT.js";
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/users", userRoutes);
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 // Exemplo rota com autenticação
 // app.use("/api/v1/posts", authenticateJWT, postRoutes);
