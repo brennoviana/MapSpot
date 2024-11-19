dd356cc5a474199408198af85c602cd4250c8cca
brennovianacoelho <brenno.viana@smartnx.com>
2024-11-19 17:40:23 -0300

    Atualizando rota de criacao usuario

--- backend/services/auth/src/users/v1/schema/userUpdateSchema.js
+++ backend/services/auth/src/users/v1/schema/userUpdateSchema.js
@@ -1,13 +1,10 @@
 import Joi from "joi";
 
 const userUpdateSchema = Joi.object({
-  name: Joi.string().max(100).optional(),
-  cpf: Joi.string().length(11).pattern(/^\d+$/).optional(),
-  address: Joi.string().max(255).optional(),
-  number: Joi.string().max(10).optional(),
-  city: Joi.string().max(100).optional(),
-  state: Joi.string().length(2).optional(),
-  zipCode: Joi.string().length(8).pattern(/^\d+$/).optional(),
+    email: Joi.string().max(100).optional(),
+    username: Joi.string().max(100).optional(),
+    cpf: Joi.string().optional(),
+    password: Joi.string().min(8).max(100).optional(),
 });
 
 export { userUpdateSchema };
