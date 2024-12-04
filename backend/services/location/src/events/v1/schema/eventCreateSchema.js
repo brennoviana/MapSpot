c9af583a0da96ee6352ae6ded869192793f3b14e
brenno.viana <brennovianacoelho@gmail.com>
2024-12-04 17:33:22 -0300

    e isso ai

--- backend/services/location/src/events/v1/schema/eventCreateSchema.js
+++ backend/services/location/src/events/v1/schema/eventCreateSchema.js
@@ -4,8 +4,8 @@ const eventCreateSchema = Joi.object({
   title: Joi.string().max(100).required(),
   description: Joi.string().optional(),
   date: Joi.date().required(),
-  location: Joi.string().optional(),
-  category: Joi.string().optional(),
+  location: Joi.string().required(),
+  category: Joi.string().required(),
 });
 
 export { eventCreateSchema };
